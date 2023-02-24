e93b2ee3fa6e1505aac637d78df3021b264ec90d
Jared Forth <jared.forth@output.com>
2023-02-24 16:05:03 -0600

    expander

--- reader.rkt
+++ reader.rkt
@@ -2,6 +2,8 @@
 (require "tokenizer.rkt" "parser.rkt")
 
 (define (read-syntax path port)
-  (datum->syntax #f `(module jsonic-mod jsonic/expander
-                       ,(parse path (make-tokenizer port)))))
+  (define parse-tree (parse path (make-tokenizer port)))
+  (define module-datum `(module jsonic-module jsonic/expander
+                          ,parse-tree))
+  (datum->syntax #f module-datum))
 (provide read-syntax)
